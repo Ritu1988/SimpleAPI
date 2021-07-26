519aaf6e3cf3975cb974df6588cceaf94c56a3b4
Ritu Chaudhary <RituC@tbotechnology.com>
2021-07-26 22:15:10 +0530

    Phase1

--- SRC/SimpleAPI/Controllers/WeatherForecastController.cs
+++ SRC/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -16,15 +16,12 @@ namespace SimpleAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+       // private readonly ILogger<WeatherForecastController> _logger;
 public WeatherForecastController()
 {}
 
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
